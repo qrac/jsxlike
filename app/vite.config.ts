14163e9d0b90c02cfcffa0246be335f65b3bb1d0
Qrac <qrac.jp@gmail.com>
2023-02-11 15:03:22 +0900

    Add preact compat to app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,4 +3,19 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "react",
+        replacement: "preact/compat",
+      },
+      {
+        find: "react-dom",
+        replacement: "preact/compat",
+      },
+    ],
+  },
+  build: {
+    //minify: false,
+  },
 })
